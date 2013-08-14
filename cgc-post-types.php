9f633d7549572b92a673ad0d9e054d1226550249
Pippin Williamson <pippin@pippinsplugins.com>
2013-08-14 12:25:08 -0500

    Undo download relations

--- cgc-post-types.php
+++ cgc-post-types.php
@@ -443,19 +443,6 @@ function cgc_post_type_relations() {
 		'sortable' => 'any'
 	) );
 
-	p2p_register_connection_type( array(
-		'name' => 'downloads_to_forums',
-		'from' => 'download',
-		'to' => 'forum',
-		'sortable' => 'any'
-	) );
-
-	p2p_register_connection_type( array(
-		'name' => 'downloads_to_posts',
-		'from' => 'download',
-		'to' => 'post',
-		'sortable' => 'any'
-	) );
 }
 
 add_action( 'p2p_init', 'cgc_post_type_relations' );
